a54187a3186718187c1c0d740e2afa1867680eb5
Atharva <atharva.nv@gmail.com>
2024-07-31 03:43:47 -0700

    added reactions to session scheduler

--- bobbert.py
+++ bobbert.py
@@ -14,6 +14,7 @@ client = discord.Client(intents=intents)
 sessionExists = False
 gameName = ""
 gameTime = ""
+neededReactions = 1
 
 # Event registers 
 @client.event
@@ -25,12 +26,16 @@ async def on_message(message):
     global sessionExists
     global gameName
     global gameTime
+    global neededReactions
 
     if message.author == client.user:
         return
 
     # The text content of the message  
     msg = message.content 
+
+    if msg == "!help":
+        await message.channel.send("need to implement")
 
     # Chat section
     if msg == "!bobbert":
@@ -69,18 +74,35 @@ async def on_message(message):
             if "at" not in msg:
                 await message.channel.send("Usage: <game> at <time>PM")
             else:
-                sessionExists = True
                 parameters = msg[11:]
                 splitParameters = parameters.split(" at ")
-                gameName = splitParameters[0].strip()
-                gameTime = splitParameters[1].strip()
-                await message.channel.send(f"Scheduled {gameName}, {gameTime}.")
+                tentativeGameName = splitParameters[0].strip()
+                tentativeGameTime = splitParameters[1].strip()
+
+                sentMessage = await message.channel.send(f"@everyone Who wants to play {tentativeGameName} @ {tentativeGameTime}?")
+
+                def check(reaction, user):
+                    return str(reaction.emoji) == '✅' and reaction.message.id == sentMessage.id
+                
+                reactionCount = 0
+
+                while reactionCount < neededReactions:
+                    reaction, user = await client.wait_for('reaction_add', check=check, timeout=None)
+                    reactionCount += 1
+                    await message.channel.send(f"We have {reactionCount} votes for {tentativeGameName}!")   
+                
+                sessionExists = True
+                gameName = tentativeGameName
+                gameTime = tentativeGameTime
+                await message.channel.send(f"Scheduled {gameName} @ {gameTime}!")              
 
     elif msg.startswith("!sessionend"):
         if(sessionExists):
-            sessionExists = False
             #remove roles 
             await message.channel.send(f"Ended/cancelled {gameName} @ {gameTime}.")
+            gameName = ""
+            gameTime = ""
+            sessionExists = False
         else:
             await message.channel.send("There's no scheduled session!")
 
